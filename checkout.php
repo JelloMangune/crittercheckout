6900c3239c4f413a6cb03b87cd0521039c8de274
JelloMangune <mangune.jello@auf.edu.ph>
2023-05-15 15:47:56 +0800

    first commit

--- checkout.php
+++ checkout.php
@@ -1,11 +1,9 @@
-<script src="https://www.paypalobjects.com/api/checkout.js"></script>
-<script src="https://checkout.stripe.com/checkout.js"></script>
-
 <?php 
 $total = 0;
     $qry = $conn->query("SELECT c.*,p.title,i.price,p.id as pid from `cart` c inner join `inventory` i on i.id=c.inventory_id inner join products p on p.id = i.product_id where c.client_id = ".$_settings->userdata('id'));
     while($row= $qry->fetch_assoc()):
         $total += $row['price'] * $row['quantity'];
+        $product = $row['title'];
     endwhile;
 ?>
 <section class="py-5">
@@ -14,9 +12,9 @@ $total = 0;
             <div class="card-body"></div>
             <h3 class="text-center"><b>Checkout</b></h3>
             <hr class="border-dark">
-            <form action="" id="place_order">
+            <form action="create-checkout-session.php" id="place_order">
                 <input type="hidden" name="amount" value="<?php echo $total ?>">
-                <input type="hidden" name="payment_method" value="cod">
+                <input type="hidden" name="payment_method" value="card">
                 <input type="hidden" name="paid" value="0">
                 <div class="row row-col-1 justify-content-center">
                     <div class="col-6">
@@ -54,88 +52,3 @@ $total = 0;
         </div>
     </div>
 </section>
-<script>
-paypal.Button.render({
-    env: 'sandbox', // change for production if app is live,
- 
-        //app's client id's
-	client: {
-        sandbox:    'AXpm8L5tVWvgKrb8A02pJLpaX41VJKxlZrqFZg9pS2hYF6VVgq2yRDO4huRupSM4TtgVr6XmFisa1Xoz',
-        //production: 'AaBHKJFEej4V6yaArjzSx9cuf-UYesQYKqynQVCdBlKuZKawDDzFyuQdidPOBSGEhWaNQnnvfzuFB9SM'
-    },
- 
-    commit: true, // Show a 'Pay Now' button
- 
-    style: {
-    	color: 'blue',
-    	size: 'small'
-    },
- 
-    payment: function(data, actions) {
-        return actions.payment.create({
-            payment: {
-                transactions: [
-                    {
-                    	//total purchase
-                        amount: { 
-                        	total: '<?php echo $total; ?>', 
-                        	currency: 'PHP' 
-                        }
-                    }
-                ]
-            }
-        });
-    },
- 
-    onAuthorize: function(data, actions) {
-        return actions.payment.execute().then(function(payment) {
-    		// //sweetalert for successful transaction
-    		// swal('Thank you!', 'Paypal purchase successful.', 'success');
-            payment_online()
-        });
-    },
- 
-}, '#paypal-button');
-
-function payment_online(){
-    $('[name="payment_method"]').val("Online Payment")
-    $('[name="paid"]').val(1)
-    $('#place_order').submit()
-}
-$(function(){
-    $('[name="order_type"]').change(function(){
-        if($(this).val() ==2){
-            $('.address-holder').hide('slow')
-        }else{
-            $('.address-holder').show('slow')
-        }
-    })
-    $('#place_order').submit(function(e){
-        e.preventDefault()
-        start_loader();
-        $.ajax({
-            url:'classes/Master.php?f=place_order',
-            method:'POST',
-            data:$(this).serialize(),
-            dataType:"json",
-            error:err=>{
-                console.log(err)
-                alert_toast("an error occured","error")
-                end_loader();
-            },
-            success:function(resp){
-                if(!!resp.status && resp.status == 'success'){
-                    alert_toast("Order Successfully placed.","success")
-                    setTimeout(function(){
-                        location.replace('./')
-                    },2000)
-                }else{
-                    console.log(resp)
-                    alert_toast("an error occured","error")
-                    end_loader();
-                }
-            }
-        })
-    })
-})
-</script>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2fcad70c4ad75b779877fab4f231d6f6ae5e0060',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2fcad70c4ad75b779877fab4f231d6f6ae5e0060',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v10.13.0',
+            'version' => '10.13.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '4ad20dd0a95b2761aecea3d2b2354cc3a99abf9b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
